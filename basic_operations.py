e1ba0792142897a38e63fbf7f3b6fcfa4459379d
romina1921 <andufeven4599@gmail.com>
2025-11-13 13:20:04 +0300

    Initial commit

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5 
+sum = number1 + number2 
+difference = number1 - number2
+multiplication= number1*number2
+print ("Addition  of ", number1, "and", number2, "is",sum )
+print("subtraction  of ", number1,  " and", number2 ,"is", difference) 
+print("multiplication  of ", number1,  " and", number2 ,"is",  multiplication)
