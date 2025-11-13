d9210aa178d38c43b1e16e632b836dc3e04b34e9
romina1921 <andufeven4599@gmail.com>
2025-11-13 13:32:45 +0300

    Added new files or updates

---
+++ future_age_calculator.py
@@ -0,0 +1,3 @@
+age = int(input("How old are you?"))
+y = age+27
+print ("In 2050, you will be " , y, "years old.")
---
+++ hours_to_seconds.py
@@ -0,0 +1,3 @@
+hours = 2
+seconds = 2*3600
+print(" 2 hours is " ,seconds,"seconds.")
