b2bfdc4dfbafff141120cd00be07e6577232e432
romina1921 <andufeven4599@gmail.com>
2025-11-15 22:51:45 +0300

    add python_introduction folder

--- future_age_calculator.py
+++
@@ -1,3 +0,0 @@
-age = int(input("How old are you?"))
-y = age+27
-print ("In 2050, you will be " , y, "years old.")
--- hours_to_seconds.py
+++
@@ -1,3 +0,0 @@
-hours = 2
-seconds = 2*3600
-print(" 2 hours is " ,seconds,"seconds.")
